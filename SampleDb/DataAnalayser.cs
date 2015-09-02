707e5690ac91c7fd88b402249cc65af326298afd
soheilsalehinia2012 <soheilsalehinia2012@yahoo.com>
2015-09-03 03:20:21 +0430

    dataAnalayzer

--- SampleDb/DataAnalayser.cs
+++ SampleDb/DataAnalayser.cs
@@ -8,23 +8,35 @@ namespace SampleDb
     {
         public static List<List<User>> CreateGroups(List<User> users, int groupSize)
         {
-            var orderedUsers = users.Zip(users.Select(user => user.TestResult.Sum(c => Char.GetNumericValue(c))),
-                (user, d) => new {User = user, Score = d})
-                .OrderBy(tuple => tuple.Score);
+            var orderedUsers = users.Zip(users.Select(user => user.TestResult
+                .Select(Char.GetNumericValue)),
+                (user, d) => new { User = user, Score = d }).ToList();
+
             var result = new List<List<User>>();
-            var room = new List<User>();
-            foreach (var user in orderedUsers)
+
+            for (int i = orderedUsers.Count; i >= groupSize; i -= groupSize)
             {
-                room.Add(user.User);
-                if (room.Count == groupSize)
-                {
-                    result.Add(room);
-                    room = new List<User>();
-                }
+                var user = orderedUsers[i-1];
+
+                var distances = new List<Tuple<double, int>>();
+                for (int j = 0; j < i; j++)
+                    // Item1 : Distance, Item2 : User Index
+                    distances.Add(Tuple.Create(user.Score
+                        .Zip(orderedUsers[j].Score, (d, d1) => Math.Abs(d1 - d)).Sum(d => d), j));
+
+                // People how the least distance with this guy, have the least distance with each other too
+                var roommates = (distances.OrderBy(tuple => tuple.Item1)
+                    .Take(groupSize)
+                    .Select(tuple => orderedUsers[tuple.Item2])).ToList();
+
+                foreach (var u in roommates)
+                    orderedUsers.Remove(u);
+
+                result.Add(roommates.Select(anon => anon.User).ToList());
             }
 
-            if (room.Count != 0)
-                result.Add(room);
+            if (orderedUsers.Any())
+                result.Add(orderedUsers.Select(anon => anon.User).ToList());
 
             return result;
         }
